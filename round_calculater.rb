29247120d8002d69cd264f2078073e21747fd77f
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-02 22:58:43 +0900

    [change] mainの入出力値を決定

--- round_calculater.rb
+++ round_calculater.rb
@@ -5,7 +5,6 @@
 # 定数
 RECOVERY_NEED_TIME = 5	# スタミナが1回復するのに必要な時間(分)
 DAY_STAMINA = (24 * 60)/RECOVERY_NEED_TIME	# 一日で回復するスタミナ
-MAX_TOTSU = 4	# 凸数の最大値
 
 END{
 	puts("終了します。。。")
@@ -78,8 +77,16 @@ puts("--------------------------------")
 puts("周回数計算機 ver. 1.0.0")
 puts("--------------------------------")
 
-need_point_num = get_need_point()
-need_round_num = get_need_round_num(need_point_num, 0, 950)
-puts()
+# 引数入力
+	# 現在のポイント数
+	# 欲しいポイント数
+	# 消費スタミナ/1出撃
+	# スタミナが1回復するのに必要な時間(分)
+	# 得るポイント数/1出撃
+	# ポイント上昇率%
 
+# 計算
 
+# 表示
+	# 必要周回数
+	# 必要日数
