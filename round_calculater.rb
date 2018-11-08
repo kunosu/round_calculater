ab2efaa5afffe7498aaf89c0b228a36eff426f5b
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-08 21:16:19 +0900

    [change] 変数名を決める

--- round_calculater.rb
+++ round_calculater.rb
@@ -2,10 +2,6 @@
 
 =end
 
-# 定数
-RECOVERY_NEED_TIME = 5	# スタミナが1回復するのに必要な時間(分)
-DAY_STAMINA = (24 * 60)/RECOVERY_NEED_TIME	# 一日で回復するスタミナ
-
 END{
 	puts("終了します。。。")
 }
@@ -31,9 +27,14 @@ def input_num(print_str = "整数値を入力: ")
 	return in_num.to_i
 end
 
+# 一日で回復するスタミナ数を得る
+def get_stamina_recover_day(recovery_need_time)
+	return (24 * 60)/recovery_need_time
+end
+
 # 同じステージを1日で何周できるか得る
-def get_round_num(need_stamina, now_stamina = 0)
-	round_num = (DAY_STAMINA + now_stamina)/need_stamina
+def get_round_num(need_stamina, now_stamina = 0, recovery_need_time)
+	round_num = (get_stamina_recover_day(recovery_need_time) + now_stamina)/need_stamina
 	
 	return round_num
 end
@@ -52,19 +53,19 @@ def get_need_point()
 end
 
 # ポイント数に応じた必要周回数を得る
-def get_need_round_num(need_point, now_point, one_round_point)
+def get_need_round_num(want_point, now_point, one_round_point)
 	need_round_num = 0
 	
-	need_round_num = (need_point - now_point)/one_round_point
+	need_round_num = (want_point - now_point)/one_round_point
 	
 	return need_round_num.ceil	# 切り上げ
 end
 
 
 # 何日かかるか求める
-def get_need_day(need_round_num)
+def get_need_day(need_round_num, recovery_need_time)
 	
-	need_day = get_round_num(20, 0)
+	need_day = get_round_num(20, 0, recovery_need_time)
 	
 	
 	return need_day
@@ -78,15 +79,20 @@ puts("周回数計算機 ver. 1.0.0")
 puts("--------------------------------")
 
 # 引数入力
-	# 現在のポイント数
-	# 欲しいポイント数
-	# 消費スタミナ/1出撃
-	# スタミナが1回復するのに必要な時間(分)
-	# 得るポイント数/1出撃
-	# ポイント上昇率%
+now_point = 0	# 現在のポイント数
+want_point = 100000	# 欲しい合計ポイント数
+one_round_point = 20	# 消費スタミナ/1周
+recovery_need_time = 5	# スタミナ回復速度(分/1スタミナ)
+get_point = 950	# 得るポイント数/1出撃
+up_point = 0	# ポイント上昇率(%)
+now_stamina = 0	# 現在のスタミナ数
 
 # 計算
 
+# 必要周回数
+need_round_num = get_need_round_num(want_point, now_point, one_round_point)
+need_day = get_need_day(need_round_num, recovery_need_time)
+
 # 表示
-	# 必要周回数
-	# 必要日数
+printf("必要周回数: %d\n", need_round_num)
+printf("必要日数: %d\n", need_day)
