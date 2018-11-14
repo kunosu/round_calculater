6f49c44e7cebca5857d66df4aeef6a9a43bb45d0
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-14 21:38:55 +0900

    [change] キーボード入力を削除

--- round_calculater.rb
+++ round_calculater.rb
@@ -11,27 +11,6 @@ END{
 	puts("\n終了します。。。\n")
 }
 
-# キーボードから整数のみを得る
-def input_num(print_str = "整数値を入力: ")
-	in_num = -1
-	
-	while true
-		print(print_str)
-		
-		in_num = gets
-
-		#printf("in_num = %s\n", in_num)
-
-		# in_numが整数以外の場合ループ
-		if in_num =~ /^-{0,1}[0-9]+$/
-			break
-		end
-	end 
-
-	# 文字列を整数値に変換して返す
-	return in_num.to_i
-end
-
 class Player
 	attr_reader :points_now				# 現在のポイント数
 	attr_reader :points_goal			# 目標のポイント数
