c50818915a4e7d7feaf7a3f60a492e56bb1d9194
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-14 22:36:29 +0900

    [add] 0除算のエラーが発生するテスト

--- round_calculater.rb
+++ round_calculater.rb
@@ -46,6 +46,15 @@ class Player_Test < Minitest::Test
 		assert_equal 105, @player.need_round_num
 	end
 
+	def test_need_round_num_ZeroDivisionError
+		player = Player.new(points_now: 10, points_goal: 100000, points_get_one_round: 0)
+
+		# 0除算のエラーが発生することを期待
+		assert_raises(ZeroDivisionError) do
+			player.need_round_num
+		end
+	end
+
 	def test_points_need
 		assert_equal 99990, @player.points_need
 	end
