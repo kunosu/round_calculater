dddb9013295bf3d109c884a6c54701d4716c0334
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-16 14:40:18 +0900

    [change] 引数を分割

--- round_calculater.rb
+++ round_calculater.rb
@@ -10,6 +10,10 @@ END{
 	puts("\n終了します。。。\n")
 }
 
-player = Player.new(points_now: 10, points_goal: 100000, points_get_one_round: 950)
+points_now = 0
+points_goal = 3000 + 6000 + 9000 + 12000 + 150000 + 400 * 11 + 3000 + 7000 + 14000
+points_get_one_round = 950 * 1.05
+
+player = Player.new(points_now: points_now, points_goal: points_goal, points_get_one_round: points_get_one_round)
 
 printf("必要周回数: %d\n", player.need_round_num)
