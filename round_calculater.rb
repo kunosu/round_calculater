4cc2d86a38e6451f171a4016a66efe197b874523
草川 <39284487+kusakawab@users.noreply.github.com>
2018-11-23 21:24:31 +0900

    [add] 標準入力の機能を追加

--- round_calculater.rb
+++ round_calculater.rb
@@ -10,14 +10,20 @@ END{
 	puts("\n終了します。。。\n")
 }
 
-# 現在のポイント数(整数値)
-points_now = 0
+def input_num(print_str)
+	printf(print_str)
 
-# 目標のポイント数(整数値)
-points_goal = 3000 + 6000 + 9000 + 12000 + 150000 + 400 * 11 + 3000 + 7000 + 14000
+	num = gets
 
-# 得るポイント数/1周(自然数)
-points_get_one_round = 950
+	# 得た文字列を数値にして返す
+	return num.to_i
+end
+
+points_now = input_num("現在のポイント数: ")
+
+points_goal = input_num("目標のポイント数: ")
+
+points_get_one_round = input_num("得るポイント数/1周: ")
 
 player = Player.new(points_now: points_now, points_goal: points_goal, points_get_one_round: points_get_one_round)
 
