cd064c6873c562470942837d2baa59dbb47cde3b
草川 <39284487+kusakawab@users.noreply.github.com>
2019-03-15 22:39:35 +0900

    Revert "Feature/2 "

--- round_calculater.rb
+++ round_calculater.rb
@@ -1,13 +1,11 @@
 # coding: UTF-8
 
-require_relative './lib/Config.rb'
 require_relative './lib/Player.rb'
 require_relative './lib/Player_factory.rb'
-require_relative './lib/Game.rb'
 
 BEGIN{
 	puts("--------------------------------")
-	puts("周回数計算機 ver. 1.2.0")
+	puts("周回数計算機 ver. 1.1.0")
 	puts("--------------------------------")	
 }
 
@@ -15,8 +13,24 @@ END{
 	puts("\n終了します。。。\n")
 }
 
-player = Player_factory.build($YAML.points)
-game = Game.new($YAML.stamina[:heal_interval])
+def input_str(print_str)
+	printf(print_str)
+
+	str = gets
+	return str
+end
+
+# 初期化用
+input_points = {:now => "現在のポイント数", 
+				:goal => "目標のポイント数", 
+				:get_one_round => "得るポイント数/1周"}
+
+points = Hash.new()
+
+input_points.each do |key, value|
+	points[key] = input_str(value + ": ")
+end
+
+player = Player_factory.build(points)
 
 puts "必要周回数: #{player.need_round_num}"
-puts "一日で回復するスタミナ: #{game.stamina_day_recover}"
