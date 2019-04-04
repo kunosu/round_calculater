9bed7d7c399ecb11688af1b2d0cec76fb2f682c7
草川 <39284487+kusakawab@users.noreply.github.com>
2019-04-04 23:42:40 +0900

    [add] #8 単体試験、実装

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,13 +1,20 @@
 class Game
-	attr_reader :heal_interval
+	attr_reader :heal_interval, :cost_one_round
 	
-	def initialize(heal_interval)
+	def initialize(heal_interval, cost_one_round)
 		@heal_interval = heal_interval	# 回復速度(分)
+		@cost_one_round = cost_one_round	# 消費スタミナ/1周
 	end
 
-	# 一日で回復するスタミナ
+	# 1日で回復するスタミナ
 	def stamina_day_recover
 		# TODO: マジックナンバーを定数にする
 		(24 * 60) / heal_interval
 	end
+
+	# 1日の周回可能回数
+	def round_num_day
+		stamina_day_recover / cost_one_round
+	end
+	
 end
--- test/Game_test.rb
+++ test/Game_test.rb
@@ -7,13 +7,19 @@ class Game_test < Minitest::Test
 	attr_reader :game
 
 	HEAL_INTERVAL = 5	# 回復間隔(分)
+	COST_ONE_ROUND = 20	# 消費スタミナ/1周
 
 	def setup
-		@game = Game.new(HEAL_INTERVAL)
+		@game = Game.new(HEAL_INTERVAL, COST_ONE_ROUND)
 	end
 
-	# 一日で回復するスタミナ
+	# 1日で回復するスタミナ
 	def test_stamina_day_recover
 		assert_equal 288, game.stamina_day_recover
+	end
+
+	# 1日の周回可能回数
+	def test_round_num_day
+		assert_equal 14, game.round_num_day
 	end
 end
