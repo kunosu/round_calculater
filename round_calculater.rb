bfe7cfbe54c5210dcea1f109930ad79ed12a50ae
草川 <39284487+kusakawab@users.noreply.github.com>
2019-04-05 22:56:47 +0900

    [change] #10 表示の調整

--- round_calculater.rb
+++ round_calculater.rb
@@ -7,7 +7,7 @@ require_relative './lib/Game.rb'
 
 BEGIN{
 	puts("--------------------------------")
-	puts("周回数計算機 ver. 1.2.0")
+	puts("周回数計算機 ver. 1.3.0")
 	puts("--------------------------------")	
 }
 
@@ -18,7 +18,12 @@ END{
 game = Game.new($YAML.stamina)
 player = Player_factory.build($YAML.points, game)
 
-puts "必要周回数: #{player.need_round_num}"
-puts "1日で回復するスタミナ: #{game.stamina_day_recover}"
-puts "1日の周回可能回数: #{game.round_num_day}"
-puts "必要なポイントを得るのにかかる日数: #{player.days_need}"
+puts "回復速度: \t\t#{$YAML.stamina[:heal_interval]} スタミナ/分"
+puts "現在のポイント数: \t#{$YAML.points[:now]} p"
+puts "目標のポイント数: \t#{$YAML.points[:goal]} p"
+puts "1周コスト: \t\t#{$YAML.stamina[:cost_one_round]} スタミナ/周"
+puts "1周の報酬: \t\t#{$YAML.points[:get_one_round]} p/周"
+
+puts "\n----------------------------------------------------"
+puts "必要周回数: #{player.need_round_num}周"
+puts "残り #{player.points_need.to_i} ポイントを得るのにかかる日数: #{player.days_need}日"
