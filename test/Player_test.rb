e4341397ef5f64b39185fe991ca3544d5775b757
草川 <39284487+kusakawab@users.noreply.github.com>
2019-04-05 22:56:11 +0900

    [change] #10 ダミークラスで単体試験を動かす

--- test/Player_test.rb
+++ test/Player_test.rb
@@ -3,20 +3,26 @@
 require 'minitest/autorun'
 require_relative '../lib/Player.rb'
 
+module Dummy_game
+	# 1日の周回可能回数
+	def self.round_num_day
+		14
+	end
+end
+
 class Player_Test < Minitest::Test
 	attr_reader :player
 
 	POINTS_NOW = 10	# 現在のポイント数
 	POINTS_GOAL = 100000	# 目標のポイント数
 	POINTS_GET_ONE_ROUND = 950	# 得るポイント数/1周
-	ROUND_NUM_DAY = 5	# 1日の周回可能回数
 
 	def setup
 		# input用のHash
 		points = {now: POINTS_NOW, goal: POINTS_GOAL, get_one_round: POINTS_GET_ONE_ROUND}
 		points = Hashie::Mash.new(points)
 
-		@player = Player.new(points, nil)
+		@player = Player.new(points, Dummy_game)
 	end
 
 	# 必要周回数
@@ -31,6 +37,6 @@ class Player_Test < Minitest::Test
 
 	# 必要なポイントを得るのにかかる日数
 	def test_days_need
-		#assert_equal 8, player.days_need
+		assert_equal 8, player.days_need
 	end
 end
